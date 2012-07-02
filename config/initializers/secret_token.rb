1e3b902d725212515cfe67956a710c6a7470883a
Johann Wilfrid-Calixte <johann@iMac-de-Anne-Sophie-Rouaux.local>
2012-07-02 11:15:11 +0200

    Initial import.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Dotfood::Application.config.secret_token = '44834eb4f277607dd27e891813f422b2af352b3454c8ec5a06fbe3ab536d9c7a2cb6dd5aab16c7eb163f6006a9111cdf9c6b64f56e5206672d646cc4694011a9'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+Dotfood::Application.config.session_store :cookie_store, key: '_dotfood_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# Dotfood::Application.config.session_store :active_record_store
