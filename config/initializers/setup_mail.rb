122ca7fc61086a6964d325a42f7374993495067b
Johann <johann@dotgee.fr>
2012-07-24 13:05:05 +0200

    params smtp

--- config/initializers/setup_mail.rb
+++ config/initializers/setup_mail.rb
@@ -2,11 +2,11 @@ ActionMailer::Base.smtp_settings = {
   :address              => "smtp.gmail.com",
   :port                 => 587,
   :domain               => "dotfood.dotgee.fr",
-  :user_name            => "dark.auroox@gmail.com",
-  :password             => "1tmby9p7",
+  :user_name            => "noreply@dotfood.dotgee.net",
+  :password             => "",
   :authentication       => :plain,
   :enable_starttls_auto => true
 }
 
-ActionMailer::Base.default_url_options[:host] = "localhost:3000"
+ActionMailer::Base.default_url_options[:host] = "dotfood.dotgee.net"
 # Mail.register_interceptor(DevelopmentMailInterceptor) if Rails.env.development?
