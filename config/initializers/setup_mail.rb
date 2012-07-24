88f945e41b9a04075de2452116635b23c68fbc83
Johann <johann@dotgee.fr>
2012-07-24 14:09:17 +0200

    reminder title

--- config/initializers/setup_mail.rb
+++ config/initializers/setup_mail.rb
@@ -1,11 +1,7 @@
 ActionMailer::Base.smtp_settings = {
-  :address              => "smtp.gmail.com",
-  :port                 => 587,
-  :domain               => "dotfood.dotgee.fr",
-  :user_name            => "noreply@dotfood.dotgee.net",
-  :password             => "",
-  :authentication       => :plain,
-  :enable_starttls_auto => true
+  :address              => "localhost",
+  :port                 => 25,
+  :domain               => "dotgee.net",
 }
 
 ActionMailer::Base.default_url_options[:host] = "dotfood.dotgee.net"
