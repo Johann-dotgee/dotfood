0771bcfcd8c9e1084e45573c55eed069ed3a478d
Johann <johann@dotgee.fr>
2012-07-25 15:13:30 +0200

    reminder

--- utils/reminder.rb
+++ utils/reminder.rb
@@ -1,3 +1,5 @@
 User.all.each do |user|
-	Reminder.reminder(user).deliver
+  if (user.find_votes {:updated_at => Date.today}.blank?) || (EatAlone.find(user.id, :conditions => {:updated_at => Date.today}).blank?) do
+	 Reminder.reminder(user).deliver
+  end
 end
